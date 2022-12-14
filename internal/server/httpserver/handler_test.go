501e5d13806321896a02556f27a94a0809901bfd
ncyellow <ncyellow@yandex.ru>
2022-12-15 01:05:38 +0300

    заменил депрекейт пакет на новый

--- internal/server/httpserver/handler_test.go
+++ internal/server/httpserver/handler_test.go
@@ -3,7 +3,7 @@ package httpserver
 import (
 	"bytes"
 	"errors"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -74,7 +74,7 @@ func runTestRequest(t *testing.T, ts *httptest.Server, method, path string, cont
 	resp, err := http.DefaultClient.Do(req)
 	require.NoError(t, err)
 
-	respBody, err := ioutil.ReadAll(resp.Body)
+	respBody, err := io.ReadAll(resp.Body)
 	require.NoError(t, err)
 
 	return resp, string(respBody)
