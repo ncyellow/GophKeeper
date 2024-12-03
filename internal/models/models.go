e8e32d77c59f9f0f58f6c9bf763c49f201afcb94
alex_kruk <ncyellow@yandex.ru>
2024-12-03 14:52:23 -0600

    update comments models.go

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,26 +1,26 @@
-// Package models содержит структуры, которые описывают наши доменные сущности
-// сейчас пока все данные строки, надо бы их заменить на корректные типы + нормальная валидация
+// Package models contains structures that describe our domain entities
+// right now all data is strings, we should replace them with correct types + proper validation
 package models
 
-// User - тип пользователя
+// User - user type
 type User struct {
 	UserID   int64  `json:"-"`
 	Login    string `json:"login"`
 	Password string `json:"password"`
 }
 
-// Card - карта банка
+// Card - bank card
 type Card struct {
 	UserID   int64  `json:"-"`
 	ID       string `json:"id"`
-	FIO      string `json:"fio"` // Написание ФИО на карте может отличаться от реального
+	FIO      string `json:"fio"` // The name on the card may differ from the actual name
 	Number   string `json:"number"`
 	Date     string `json:"date"`
 	CVV      string `json:"cvv"`
 	MetaInfo string `json:"metainfo"`
 }
 
-// Text - текстовый контент
+// Text - text content
 type Text struct {
 	UserID   int64  `json:"-"`
 	ID       string `json:"id"`
@@ -28,7 +28,7 @@ type Text struct {
 	MetaInfo string `json:"metainfo"`
 }
 
-// Binary - бинарные данные
+// Binary - binary data
 type Binary struct {
 	UserID   int64  `json:"-"`
 	ID       string `json:"id"`
@@ -36,7 +36,7 @@ type Binary struct {
 	MetaInfo string `json:"metainfo"`
 }
 
-// Login - данные по логинам
+// Login - login data
 type Login struct {
 	UserID   int64  `json:"-"`
 	ID       string `json:"id"`
